e822d006150bdc7b6f03c7dd9c459bbe9d46f1c5
anekil <78557681+anekil@users.noreply.github.com>
2023-07-27 10:54:18 +0200

    working on calling API

--- backend/src/Controller/GamesAPIController.php
+++ backend/src/Controller/GamesAPIController.php
@@ -11,18 +11,38 @@ class GamesAPIController extends AbstractController
 {
     #[Route('/get', name: 'app_get_api')]
     public function askAPI() : JsonResponse {
-        $request = json_decode('{
-            "mechanics": "vZsDDAdOoe,WPytek5P8l",
-            "categories": "hBqZ3Ar4RJ,eX8uuNlQkQ",
-            "min_players": 2,
-            "max_players": 4,
-            "min_playtime": 10,
-            "max_playtime": 30,
-            "min_age": 10,
-            "fields": "id,name,min_players,max_players,min_playtime,max_playtime,min_age,description,image_url,mechanics,categories,rules_url,average_user_rating,description_preview"
-            }');
+        $fromFront = json_decode('{
+            "mechanics": [
+                "fBOTEBUAmV"        
+              ],
+            "categories": [
+                "7rV11PKqME",
+                "nuHYRFmMjU"
+              ],
+            "min_players": 2
+            }', TRUE);
+        $toAPI = array();
 
-        $url = 'https://api.boardgameatlas.com/api/search?limit=1&pretty=true&client_id=86dlh7CuWH&random=true&fields=id%2Cname%2Cmin_players%2Cmax_players%2Cmin_playtime%2Cmax_playtime%2Cmin_age%2Cdescription%2Cimage_url%2Cmechanics%2Ccategories%2Crules_url%2Caverage_user_rating%2Cdescription_preview';
+        dump($fromFront);
+
+        $toAPI["mechanics"] = implode(',', $fromFront["mechanics"]);
+        unset($fromFront["mechanics"]);
+
+        $toAPI["categories"] = implode(',', $fromFront["categories"]);
+        unset($fromFront["categories"]);
+
+        foreach ($fromFront as $key => $value) {
+            $toAPI[$key] = $value;
+        }
+
+        dump($toAPI);
+
+        $url = 'https://api.boardgameatlas.com/api/search?limit=1&pretty=true&client_id=86dlh7CuWH&fields=id,name,min_players,max_players,min_playtime,max_playtime,min_age,description,image_url,mechanics,categories,rules_url,average_user_rating,description_preview';
+        foreach($toAPI as $key => $value){
+            $url .= '&'.$key.'='.$value;
+        }
+        dump($url);
+        # $url = 'https://api.boardgameatlas.com/api/search?limit=1&pretty=true&client_id=86dlh7CuWH&random=true&fields=id%2Cname%2Cmin_players%2Cmax_players%2Cmin_playtime%2Cmax_playtime%2Cmin_age%2Cdescription%2Cimage_url%2Cmechanics%2Ccategories%2Crules_url%2Caverage_user_rating%2Cdescription_preview';
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
@@ -36,6 +56,8 @@ class GamesAPIController extends AbstractController
             CURLOPT_CUSTOMREQUEST => 'GET',
         ));
         $response = curl_exec($curl);
+        if($response !== false)
+            $response = json_decode($response);
         curl_close($curl);
         return $this->json($response);
     }
