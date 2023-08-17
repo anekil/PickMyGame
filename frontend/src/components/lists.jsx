05837a212cbc38f3ac168898524f0ae4ba6c79f0
anekil <78557681+anekil@users.noreply.github.com>
2023-08-17 20:49:45 +0200

    changed list to group of checkboxes and configured tailwind css

--- frontend/src/components/lists.jsx
+++ frontend/src/components/lists.jsx
@@ -1,7 +1,11 @@
 'use client'
 import swr from 'swr'
 import axios from "axios";
+import React from "react";
+import {CheckboxGroup, Checkbox} from "@nextui-org/react";
 
+
+// TODO docker address
 const fetcher = axios.create({ baseURL: 'http://127.0.0.1:8000/api/'});
 
 export const listItems = async (handle) => {
@@ -11,23 +15,34 @@ export const listItems = async (handle) => {
 
 export const ApiList = (handle) =>  {
     handle = handle["option"] + '?page=1';
-    console.log(handle);
     const {data: items, isLoading, error} = swr(handle, listItems);
 
     if (error) return "An error has occurred";
     if (isLoading) return "Loading...";
-
-    console.log(items);
-
-    const result = items.map(item =>
-        <li
-            key={item.api_id}
-        >
-            {item.name}
-        </li>
+    const result  = items.map(item =>
+        <Checkbox value={item.api_id}>{item.name}</Checkbox>
     );
 
     return (
-        <ul>{result}</ul>
+        <>{result}</>
+    );
+}
+
+export default function App(option) {
+    option = option["option"];
+    const [groupSelected, setGroupSelected] = React.useState([]);
+
+    return (
+        <div className="flex flex-col gap-3">
+            <p className="text-default-500">Selected: {groupSelected.join(",")}</p>
+            <CheckboxGroup
+                label={"Select " + option}
+                color="warning"
+                value={groupSelected}
+                onValueChange={setGroupSelected}
+            >
+                { ApiList(option) }
+            </CheckboxGroup>
+        </div>
     );
 }
