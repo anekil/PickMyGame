dbabcfd22843a4f272f8665a5ba32818a778f8ea
anekil <78557681+anekil@users.noreply.github.com>
2023-09-03 20:31:54 +0200

    added about page

---
+++ frontend/src/components/gameData.jsx
@@ -0,0 +1,34 @@
+
+export const GameData = ({ gameString }) => {
+    let data = JSON.parse(gameString);
+
+    return (
+        <div>
+            <h1>{data.name}</h1>
+            <img src={data.image_url} alt={"data image"} width={120} height={120}/>
+            <p>Players: {data.min_players}-{data.max_players}</p>
+            <p>Playtime: {data.min_playtime}-{data.max_playtime}</p>
+            { data.average_user_rating != null
+                ? <p>User rating: {data.average_user_rating}</p> : <></>
+            }
+            <ul>
+                <p>Categories:</p>
+                {data.categories.map(item =>
+                    <li key={item}>{item}</li>
+                )}
+            </ul>
+            <ol>
+                <p>Mechanics:</p>
+                {data.mechanics.map(item =>
+                    <li key={item}>{item}</li>
+                )}
+            </ol>
+            { data.description != null
+                ? <p>Description: {data.description}</p> : <></>
+            }
+            { data.rules_url != null
+                ? <a href={data.rules_url}>Link to rules</a> : <></>
+            }
+        </div>
+    );
+}
