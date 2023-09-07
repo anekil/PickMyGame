8e5d1418bd65e2179c0deca0f8766448abde0c23
anekil <78557681+anekil@users.noreply.github.com>
2023-09-07 23:23:52 +0200

    fixed page about game

--- frontend/src/components/gameData.jsx
+++ frontend/src/components/gameData.jsx
@@ -8,54 +8,71 @@ export const GameData = () => {
     return (
         <div>
             <h1>{data.name}</h1>
-            { /*data.cover.url != null
-                ? <img src={data.image_url} alt={"data image"} width={120} height={120}/> : <></>*/
+
+            { data.cover != null
+                ? <img src={data.image_url} alt={"data image"} width={120} height={120}/> : <></>
             }
+
             { data.summary != null
                 ? <p>Summary: {data.summary}</p> : <></>
             }
-            <p>Rating: {data.total_rating}</p>
-            <ul>
-                <p>Categories:</p>
-                {data.genres.map(item =>
-                    <li key={item}>{item}</li>
-                )}
-            </ul>
-            <ol>
-                <p>Themes:</p>
-                {data.themes.map(item =>
-                    <li key={item}>{item}</li>
-                )}
-            </ol>
-            <ul>
-                <p>Keywords:</p>
-                {data.keywords.map(item =>
-                    <li key={item}>{item}</li>
-                )}
-            </ul>
-            <ul>
-                <p>Platforms:</p>
-                {data.platforms.map(item =>
-                    <li key={item}>{item}</li>
-                )}
-            </ul>
 
-            { data.multiplayer_modes != null
-                    ?   <div>
-                        <p>onlinemax: {data.multiplayer_modes.onlinemax}</p>
-                        <p>offlinemax: {data.multiplayer_modes.offlinemax}</p>
-                        <p>campaigncoop: {data.multiplayer_modes.campaigncoop}</p>
-                        <p>lancoop: {data.multiplayer_modes.lancoop}</p>
-                        <p>offlinecoop: {data.multiplayer_modes.offlinecoop}</p>
-                        <p>onlinecoop: {data.multiplayer_modes.onlinecoop}</p>
-                    </div>
-                    : <></>
+            { data.total_rating != null
+                ?  <p>Rating: {data.total_rating}</p> : <></>
             }
 
-            <p>Screenshots:</p>
-            {data.screenshots.map((item, index) => (
-                <img key={index} src={item.url} alt={`screenshot-${index}`} width={120} height={120} />
-            ))}
+            { data.genres != null ?
+                <ul>
+                    <p>Genres:</p>
+                    {data.genres.map(item =>
+                        <li key={item}>{item}</li>
+                    )}
+                </ul> : <></>
+            }
+            { data.themes != null ?
+                <ul>
+                    <p>Themes:</p>
+                    {data.themes.map(item =>
+                        <li key={item}>{item}</li>
+                    )}
+                </ul> : <></>
+            }
+            { data.keywords != null ?
+                <ul>
+                    <p>Keywords:</p>
+                    {data.keywords.map(item =>
+                        <li key={item}>{item}</li>
+                    )}
+                </ul> : <></>
+            }
+            { data.platforms != null ?
+                <ul>
+                    <p>Platforms:</p>
+                    {data.platforms.map(item =>
+                        <li key={item}>{item}</li>
+                    )}
+                </ul> : <></>
+            }
+
+            { data.multiplayer_modes != null ?
+                <div>
+                    <p>onlinemax: {data.multiplayer_modes.onlinemax}</p>
+                    <p>offlinemax: {data.multiplayer_modes.offlinemax}</p>
+                    <p>campaigncoop: {data.multiplayer_modes.campaigncoop}</p>
+                    <p>lancoop: {data.multiplayer_modes.lancoop}</p>
+                    <p>offlinecoop: {data.multiplayer_modes.offlinecoop}</p>
+                    <p>onlinecoop: {data.multiplayer_modes.onlinecoop}</p>
+                </div> : <></>
+            }
+
+            { data.screenshots != null ?
+                <a>
+                    <p>Screenshots:</p>
+                    {data.screenshots.map((item, index) => (
+                        <img key={index} src={item.url} alt={`screenshot-${index}`} width={120} height={120} />
+                    ))}
+                </a> : <></>
+            }
 
             { data.url != null
                 ? <a href={data.url}>Link to {data.name}'s page</a> : <></>
